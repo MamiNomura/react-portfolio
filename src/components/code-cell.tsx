5c0cea1412556912e2bc0dc4fc17cade66faf46b
Mami Nomura <mami@mamis-air.lan>
2023-06-24 19:53:21 -0700

    Added show() function to the code editor so that user can easily show the content of the code in the preview window

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -7,6 +7,7 @@ import Resizable from "./resizable";
 import { Cell } from "../state";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/new-type-selector";
+import { useCumulativeCode } from "../hooks/use-cumulative-code";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
@@ -18,14 +19,15 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
       return;
     }
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
     }, 750);
 
     return () => {
@@ -34,7 +36,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     // eslint warns us to add bundle to the list but if we do
     // it will go to the infinite loop
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.id, cell.content, createBundle]);
+  }, [cell.id, cumulativeCode, createBundle]);
 
   return (
     <Resizable direction="vertical">
